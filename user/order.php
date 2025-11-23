27c3c7cf73e2850e90c3a115564095efd24523da
RafkaJulianee <rafkagenggkk@gmail.com>
2025-11-23 19:40:58 +0700

    update

--- user/order.php
+++ user/order.php
@@ -93,6 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Checkout - ZIFOOD</title>
+    <link rel="stylesheet" href="CSS/order.css">
     <link rel="shortcut icon" href="img/zifood.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
